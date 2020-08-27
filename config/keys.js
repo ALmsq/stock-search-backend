30c2e920c4b88a9c69c2a7671e8c712863c983c5
ALmsq <alexander.mosquea3@gmail.com>
2020-08-26 20:52:14 -0400

    patched key vulnerability

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://alex:5626373As@cluster0.5qt1x.mongodb.net/stock-search?retryWrites=true&w=majority", 
+   
     secretOrKey: "secret"
   };
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require('passport')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const users = require('./routes/api/users')
 
@@ -15,11 +17,14 @@ app.use(
 );
 app.use(bodyParser.json());
 // DB Config
-const db = require("./config/keys").mongoURI;
+// const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
+
+const URI = process.env.MONGODB_URI || process.env.URI
+
 mongoose
   .connect(
-    db,
+    URI,
     { useNewUrlParser: true }
   )
   .then(() => console.log("MongoDB successfully connected"))
@@ -33,6 +38,8 @@ mongoose
 
   //Routes
   app.use('/api/users', users)
+
+   
 
   
   const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
